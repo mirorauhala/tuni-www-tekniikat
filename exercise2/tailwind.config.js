37c281a98d8c0a7bdbf0b8ffd4ce30bec5e78596
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-03-15 14:31:49 +0200

    add tailwindcss

---
+++ exercise2/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      'sans': ['Inter'],
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
